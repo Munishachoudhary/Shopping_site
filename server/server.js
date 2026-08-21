2b215c30fe693ebfb1bda26f8ff3461fc7f53dea
Munishachoudhary <munishachoudhary15@gmail.com>
2026-08-21 21:25:01 +0530

    Fix production CORS

--- server/server.js
+++ server/server.js
@@ -27,13 +27,10 @@ const allowedOrigins = [
 
 app.use(
   cors({
-    origin: function (origin, callback) {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
+    origin: allowedOrigins,
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
 app.use(express.json());
